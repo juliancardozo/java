d2eb5365ccd042e193a2320c9ecdbf59bace14bc
Julian Cardozo <jcardozo@st.com.uy>
2022-01-10 10:37:37 -0300

    First commit

---
+++ src/main/java/com/example/javarepository/JavaRepositoryApplication.java
@@ -0,0 +1,15 @@
+package com.example.javarepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRepositoryApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JavaRepositoryApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/example/javarepository/JavaRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javarepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
